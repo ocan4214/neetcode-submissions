8297e55c44b52f64068f5269c60a313c707b692c
Onur <hasanonurcansarihan@gmail.com>
2026-05-19 18:38:23 +0300

    Add: insertionSort - submission-3

---
+++ Data Structures & Algorithms/insertionSort/submission-3.js
@@ -0,0 +1,35 @@
+/**
+ * Pair class to store key-value pairs
+ */
+// class Pair {
+//     /**
+//      * @param {number} key The key to be stored in the pair
+//      * @param {string} value The value to be stored in the pair
+//      */
+//     constructor(key, value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[][]}
+     */
+    sortedPairs =[];
+    insertionSort(pairs) {
+        
+        //0. index sorted kabul edilecek.
+        for(let i=0;i<pairs.length;i++){
+            let unsortedKey = pairs[i]
+            let j = i-1;
+            while(j>=0 && pairs[j].key > unsortedKey.key ){
+                pairs[j+1] = pairs[j]
+                j--;
+            }
+            pairs[j+1]  = unsortedKey
+            this.sortedPairs.push(JSON.parse(JSON.stringify(pairs)));
+        }
+        return this.sortedPairs
+    }
+}
